2d7fd64fadc65bb2b08b977587f493697c619b31
Ryan <ryan1cosheril@gmail.com>
2018-10-02 12:44:53 +0100

    did more stuff wow

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,3 +1,25 @@
 public class VendingMachine {
     private int numTokens, numCans;
+
+    public VendingMachine()
+    {
+        setNumTokens(0);
+        setNumCans(0);
+    }
+
+    public void setNumTokens(int numTokens) {
+        this.numTokens = numTokens;
+    }
+
+    public int getNumTokens() {
+        return numTokens;
+    }
+
+    public void setNumCans(int numCans) {
+        this.numCans = numCans;
+    }
+
+    public int getNumCans() {
+        return numCans;
+    }
 }
